7183b54ecfa32646bde6a0cb80bba64e0a7753fa
Randi Perera <randiperera2003@gmail.com>
2025-12-27 12:46:07 +0530

    Added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+export default function Footer() {
+    return (
+      <footer className="footer">
+        <div>
+          <h3>HomeHarbor Estate Agents</h3>
+          <p>Helping you find your perfect home.</p>
+        </div>
+  
+        <div>
+          <p>📍 London, UK</p>
+          <p>📞 +44 20 7946 0958</p>
+          <p>✉ info@homeharbor.co.uk</p>
+        </div>
+  
+        <p className="copyright">
+          © 2026 HomeHarbor. All rights reserved.
+        </p>
+      </footer>
+    );
+  }
+  
