31b807e712153818ad9fe33a20c8fc0161745343
Randi Perera <randiperera2003@gmail.com>
2026-01-05 01:35:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/estate-agent-app/', // IMPORTANT: Add this line
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.js',
-  }
-})
+  base: "/bluebrink/"
+});
